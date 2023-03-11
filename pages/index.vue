f7681d703cf6bae3cfb621704295067ab5e89ee8
warflash <nwiesinger67@gmail.com>
2023-03-11 17:33:52 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img src="/logo.svg" />
+        <div>
+            <span>
+                Hello World
+            </span>
+            <NuxtLink to="/nested">-> Nested</NuxtLink>
+            <span>
+                API: {{ apiData.data }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const apiData = useFetch("/api/hello", {
+    server: false,
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
